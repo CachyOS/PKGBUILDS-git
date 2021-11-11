edc155a3ff9fec40feae895f3b95041066c910ae
P. Jung <ptr1337@cachyos.org>
2021-11-11 22:14:41 +0000

    -git weekly builds

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+files=$(find . -name "PKGBUILD")
+
+for f in $files
+do
+        d=$(dirname $f)
+        echo "makepkg -src --sign --skipinteg --noconfirm $f"
+        cd $d
+        docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg:latest
+        docker rm dockerbuild
+        cd ..
+done
+
+mv */*.tar.zst* /home/ptr1337/packages/
