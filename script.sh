fbf86f42f6215cbac97c960aecb315481eca9f7f
ptr1337 <ptr1337@cachyos.org>
2022-02-06 14:28:15 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-paru -G adwaita-qt-git \
-ant-dracula-kde-theme-git \
+rm -rf *-git
+
+paru -G ant-dracula-kde-theme-git \
 ant-dracula-theme-git \
 apparmor-git \
-apple-music-electron-git \
 arch-audit-git \
-attica-git \
-baloo-git \
-birdtray-git \
 bleachbit-git \
-bluedevil-git \
 breeze-git \
 breeze-icons-git \
 ckb-next-git \
@@ -24,29 +20,19 @@ ffmpeg-full-git \
 findex-git \
 firejail-git \
 fish-git \
-frameworkintegration-git \
 ftxui-git \
-git-git \\
-kwidgetsaddons-git \
+git-git \
 kwin-bismuth-git \
-kwindowsystem-git \
 kwinft-git \
-kwin-git \
-kxmlgui-git \
-kxmlrpcclient-git \
 lib32-mesa-git \
-libksysguard-git \
 libretro-fbneo-git \
 lightly-git \
 llvm-git \
 mesa-git \
-modemmanager-qt-git \
-networkmanager-qt-git \
 noisetorch-git \
 openrazer-git \
 openrgb-git \
 pacui-git \
-phoronix-test-suite-git \
 pipewire-full-git \
 kwinft-git \
 proton \
@@ -64,7 +50,6 @@ files=$(find . -name "PKGBUILD")
 for f in $files
 do
         d=$(dirname $f)
-        echo "makepkg -src --sign --skipinteg --noconfirm $f"
         cd $d
         docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache:/home/notroot/ccache pttrr/docker-makepkg
         docker rm dockerbuild
